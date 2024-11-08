327a0d925b5ca1e8a7260a690265c666bf1da3c0
MogesBekele <mogesbekele32@gmail.com>
2024-11-08 14:33:18 +0300

    import modified

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import './About.css'
 import about_img from '../../assets/photo.jpg'
-import play_icon from '../../assets/play-icon.png'
 const About = () => {
   return (
     <div className='about'>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import './Navbar.css'
-import logo from '../../assets/logo.png'
+
 import { GiSuspensionBridge } from "react-icons/gi";
 import menu_icon from '../../assets/menu-icon.png'
 import { Link } from 'react-scroll'
